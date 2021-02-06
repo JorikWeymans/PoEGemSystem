6f51aea823b4d8b925459c84fcc2b3237c668cb2
Jorik Weymans <jorikweymans@hotmail.be>
2021-02-06 18:34:10 +0100

    added basic skill panel

---
+++ Assets/Scripts/UI/Panels/SkillPanel.cs
@@ -0,0 +1,34 @@
+﻿//Created by Jorik Weymans 2020
+
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Jorik
+{
+	public sealed class SkillPanel : UIPanel
+	{
+        public static readonly string Name = "MessagePanel";
+
+        [SerializeField] private TMP_Text _Text = null;
+
+        private MessagePanelParams _myParams;
+        protected override void InternalAwake()
+        {
+
+        }
+        protected override void OnOpen(PanelParams parameters)
+        {
+        }
+
+        protected override void InternalUpdate(float elapsed)
+        {
+            //
+        }
+
+        private void CloseWithAnswer(DialogResult answer)
+        {
+            _myParams.AnswerHandler?.Invoke(answer);
+        }
+    }
+}
